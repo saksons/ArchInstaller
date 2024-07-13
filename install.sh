86236eb42c69db799a2dbec5a9f940797559281c
Ваше Имя <daniel>
2024-07-13 21:13:43 +0300

    update parting

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
-echo $(lsblk)
+lsblk
 
-read disk
+read -p "Укажите диск: " disk
+read -p "Продожить? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 parted /dev/$disk mklabel gpt
 
 echo "[+] EFI"
-parted -a opt /dev/$disk mkpart primary ext4 0% 1GB
+parted -a opt /dev/$disk mkpart primary ext4 0% 1024MiB > /dev/null
 echo "[+] SWAP"
-parted -a opt /dev/$disk mkpart primary ext4 1GB 5GB
+parted -a opt /dev/$disk mkpart primary ext4 1024MiB 5120MiB > /dev/null
 echo "[+] VAR"
-parted -a opt /dev/$disk mkpart primary ext4 5GB 16GB
+parted -a opt /dev/$disk mkpart primary ext4 5MiB 16384MiB > /dev/null
 echo "[+] HOME"
-parted -a opt /dev/$disk mkpart primary ext4 16GB 40%
+parted -a opt /dev/$disk mkpart primary ext4 16384MiB 40% > /dev/null
 echo "[+] ROOT"
-parted -a opt /dev/$disk mkpart primary ext4 40% 100%
+parted -a opt /dev/$disk mkpart primary ext4 40% 100% > /dev/null
