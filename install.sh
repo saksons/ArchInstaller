9695612c83aff24da66b7a3d1b4240783f7f830e
Ваше Имя <daniel>
2024-07-13 21:04:02 +0300

    update parting

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ read disk
 parted /dev/$disk mklabel gpt
 
 echo "[+] EFI"
-parted -a opt /dev/$disk mkpart primary ext4 0 2097152
+parted -a opt /dev/$disk mkpart primary ext4 0% 1GB
 echo "[+] SWAP"
-parted -a opt /dev/$disk mkpart primary ext4 2097153 10485761
+parted -a opt /dev/$disk mkpart primary ext4 1GB 5GB
 echo "[+] VAR"
-parted -a opt /dev/$disk mkpart primary ext4 10485761 31457282
+parted -a opt /dev/$disk mkpart primary ext4 5GB 16GB
 echo "[+] HOME"
-parted -a opt /dev/$disk mkpart primary ext4 40%
+parted -a opt /dev/$disk mkpart primary ext4 16GB 40%
 echo "[+] ROOT"
-parted -a opt /dev/$disk mkpart primary ext4 100%
+parted -a opt /dev/$disk mkpart primary ext4 40% 100%
