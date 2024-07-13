7ae8a07f4a6c07ad69adbffa38a427bec92c1b2d
Ваше Имя <daniel>
2024-07-13 21:21:54 +0300

    update parting

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 lsblk
 
-read -p "Укажите диск: " disk
+read -p "choose disk: " disk
 read -p "Продожить? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-parted /dev/$disk mklabel gpt
+parted /dev/$disk mklabel gpt > /dev/null 2>&1
 
 echo "[+] EFI"
-parted -a opt /dev/$disk mkpart primary ext4 0% 1024MiB > /dev/null
-echo "[+] SWAP"
-parted -a opt /dev/$disk mkpart primary ext4 1024MiB 5120MiB > /dev/null
-echo "[+] VAR"
-parted -a opt /dev/$disk mkpart primary ext4 5MiB 16384MiB > /dev/null
-echo "[+] HOME"
-parted -a opt /dev/$disk mkpart primary ext4 16384MiB 40% > /dev/null
-echo "[+] ROOT"
-parted -a opt /dev/$disk mkpart primary ext4 40% 100% > /dev/null
+parted -a opt /dev/$disk mkpart primary ext4 0MiB 1024MiB > /dev/null 2>&1
+echo "[+] swap"
+parted -a opt /dev/$disk mkpart primary ext4 1024MiB 5120MiB > /dev/null 2>&1
+echo "[+] var"
+parted -a opt /dev/$disk mkpart primary ext4 5120MiB 16384MiB > /dev/null 2>&1
+echo "[+] home"
+parted -a opt /dev/$disk mkpart primary ext4 16384MiB 40% > /dev/null 2>&1
+echo "[+] root"
+parted -a opt /dev/$disk mkpart primary ext4 40% 100% > /dev/null 2>&1
+
+parted /dev/sda print
